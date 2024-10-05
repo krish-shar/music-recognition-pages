b2353325ff47182ce70bbb0915e1cc02765da002
krish-shar <krishsharma2308@gmail.com>
2024-10-04 23:57:03 -0400

    edited deployment config

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,10 @@ const nextConfig = {
     output: "export",
     images: {
       unoptimized: true,
-    }
+    },
+    // this is deployed on github pages, so we need to set the basePath
+    basePath: "/music-predictor-pages/",
+    assetPrefix: "/music-predictor-pages/",
   };
   
 export default nextConfig;
